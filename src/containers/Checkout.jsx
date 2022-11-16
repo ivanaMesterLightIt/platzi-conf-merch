727bd23f676844bd86cdc73e7cfc8f2ad1904ef8
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-16 13:47:32 -0300

    progress class 24

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../styles/components/Checkout.css'
 
 const Checkout = () => {
     return (
-        <h1>Checkout</h1>
+        <div className="Checkout">
+            <div className="Checkout-content">
+                <h3>Lista de pedidos</h3>
+                <div className="Checkout-item">
+                    <div className="Checkout-element">
+                        <h4>Item name</h4>
+                        <span>$100</span>
+                    </div>
+                    <button type='button'>Eliminar</button>
+                </div>
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Precio total: $100</h3>
+                <button type='button'>Continuar pedido</button>
+            </div>
+        </div>
     );
 }
 
