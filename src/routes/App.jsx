ef7ee1cef8ef5e7f703d5d435ca9379d4c438fe7
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-16 12:46:58 -0300

    progress class 22

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '../components/Layout';
 import Checkout from '../containers/Checkout';
 import Home from '../containers/Home';
 import Information from '../containers/Information';
@@ -10,14 +11,16 @@ import Success from '../containers/Success';
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/checkout' element={<Checkout />} />
-                <Route path='/checkout/information' element={<Information />} />
-                <Route path='/checkout/payment' element={<Payment />} />
-                <Route path='/checkout/success' element={<Success />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/checkout' element={<Checkout />} />
+                    <Route path='/checkout/information' element={<Information />} />
+                    <Route path='/checkout/payment' element={<Payment />} />
+                    <Route path='/checkout/success' element={<Success />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 }
